e0ec729a4b42d300da75ffc4dbd70b93aeadfc2c
David Hutcheson <themacmini@hutcheson-mini.lan>
2024-01-29 22:06:04 +0000

    todo demo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React, { Component } from "react";
 
 class App extends Component {
-  state = {};
+  state = { todos: ["Clean Car", "Buy bread", "Feed fish"] };
+
+  onTodoInput = (e) => {
+    this.setState({ todoInput: e.target.value });
+  };
+
+  onAddClick = () => {
+    const todos = [...this.state.todos];
+    todos.push(this.state.todoInput);
+    this.setState({ todos });
+  };
+
   render() {
-    return <p>TEST</p>;
+    console.log(this.state);
+    const { todos } = this.state;
+    return (
+      <>
+        <input type="text" onInput={this.onTodoInput} />
+        <button onClick={this.onAddClick}>Add</button>
+        {todos.map((todo) => {
+          return <p>{todo}</p>;
+        })}
+      </>
+    );
   }
 }
 
