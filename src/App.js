e970e002aee81c5246fef2427689cfb01f7f8933
David Hutcheson <d.hutcheson@hotmail.co.uk>
2024-06-14 14:30:11 +0100

    State completion

--- src/App.js
+++ src/App.js
@@ -2,11 +2,41 @@ import React, { Component } from "react";
 import Todo from "./components/Todo";
 
 class App extends Component {
-  state = { todos: ["Clean car", "Buy milk"] };
+  state = {
+    todos: [
+      { title: "Clean car", done: false },
+      { title: "Buy milk", done: true },
+      { title: "Buy Cheese", done: false },
+    ],
+  };
+
+  onToggleDone = (item) => {
+    const index = this.state.todos.findIndex((todo) => todo.title === item);
+    const todos = [...this.state.todos];
+    todos[index].done = !todos[index].done;
+    this.setState({ todos });
+  };
+
   render() {
-    return this.state.todos.map((todo) => {
-      return <Todo todo={todo} />;
+    let count = 0;
+    this.state.todos.forEach((todo) => {
+      if (todo.done) count++;
     });
+    return (
+      <>
+        <p>Total tasks done:{count}</p>
+        {this.state.todos.map((todo) => {
+          return (
+            <Todo
+              todo={todo.title}
+              done={todo.done}
+              onToggleDone={this.onToggleDone}
+            />
+          );
+        })}
+        ;
+      </>
+    );
   }
 }
 
