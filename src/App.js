7a8a671f0b6a43cf4cd86fdd107050214bf45934
David Hutcheson <d.hutcheson@hotmail.co.uk>
2024-06-28 11:59:50 +0100

    validation with joi

--- src/App.js
+++ src/App.js
@@ -1,20 +1,61 @@
 import React, { Component } from "react";
-import Child from "./components/Child";
+import Joi from "joi";
 
 class App extends Component {
-  state = { show: true };
+  state = {};
+
+  schema = {
+    name: Joi.string().min(3).max(10),
+    username: Joi.string().min(5),
+    email: Joi.string()
+      .email({ tlds: { allow: false } })
+      .required(),
+  };
+
+  onUserInput = async (e) => {
+    const userInput = { ...this.state.userInput };
+    userInput[e.target.id] = e.target.value;
+
+    this.setState({ userInput });
+
+    const _joiInstance = Joi.object(this.schema);
+
+    try {
+      await _joiInstance.validateAsync(this.state.userInput);
+      this.setState({ errors: undefined });
+    } catch (e) {
+      // console.log(e);
+
+      const errorsMod = {};
+      e.details.forEach((error) => {
+        errorsMod[error.context.key] = error.message;
+      });
+      this.setState({ errors: errorsMod });
+    }
+  };
+
   render() {
+    console.log(this.state);
     return (
-      <>
-        <button
-          onClick={() => {
-            this.setState({ show: !this.state.show });
-          }}
-        >
-          Toggle
-        </button>
-        {this.state.show && <Child />}
-      </>
+      <div>
+        <form onInput={this.onUserInput}>
+          <div>
+            <label for="name">Name</label>
+            <input type="text" id="name" name="name" />
+            <p>{this.state.errors && this.state.errors.name}</p>
+          </div>
+          <div>
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" />
+            <p>{this.state.errors && this.state.errors.email}</p>
+          </div>
+          <div>
+            <label for="username">Username</label>
+            <input type="text" id="username" name="username" />
+            <p>{this.state.errors && this.state.errors.username}</p>
+          </div>
+        </form>
+      </div>
     );
   }
 }
