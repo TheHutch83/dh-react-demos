a3b29c48c37314b9ea3e2e86bc61b7a8a3debddd
David Hutcheson <davidhutcheson@MacBook-Air.local>
2024-07-01 13:44:45 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import React from "react";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
+import Error from "./pages/Error";
+import Nav from "./pages/Nav";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="about" element={<About />} />
-      <Route path="contact" element={<Contact />} />
-    </Routes>
+    <>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact/:country" element={<Contact />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </>
   );
 };
 
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,17 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const Contact = () => {
+  const { country } = useParams();
+
+  if (country === "en") {
+    return <p>Please call 019120304040</p>;
+  }
+
+  if (country === "usa") {
+    return <p>Please call 555 123 1456</p>;
+  }
+
   return <div>Contact page</div>;
 };
 
