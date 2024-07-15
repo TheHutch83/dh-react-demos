d99fb953961deaee25588c24d6790cbf196ad8f7
David Hutcheson <davidhutcheson@macbook-air.home>
2024-07-15 20:54:55 +0200

    functional intro branch created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,7 @@
 import React from "react";
 
-import { useState } from "react";
-
 const App = () => {
-  const [count, setCount] = useState(0);
-
-  const add = () => {
-    setCount(count + 1);
-  };
-
-  const minus = () => {
-    setCount(count - 1);
-  };
-
-  return (
-    <>
-      <button onClick={add}>+</button>
-      <button onClick={minus}>-</button>
-      <p>Count: {count}</p>
-      {/* this count here, refers to count above */}
-    </>
-  );
+  return <p>Hello World</p>;
 };
 
 export default App;
