3ec225cc48e1b3505620d3bad9869ad058f41c84
David Hutcheson <davidhutcheson@MacBook-Air.local>
2024-07-16 18:22:43 +0200

    use effect simple demo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import React, { Component } from "react";
-import Child from "./components/Child";
+import React, { useEffect, useState } from "react";
 
-class App extends Component {
-  state = { show: true };
-  render() {
-    return (
-      <>
-        <button
-          onClick={() => {
-            this.setState({ show: !this.state.show });
-          }}
-        >
-          Toggle
-        </button>
-        {this.state.show && <Child />}
-      </>
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [otherCount, setOtherCount] = useState(0);
+
+  useEffect(() => {
+    console.log(
+      "useEffect ran - like componentDidMount and componentDidUpdate"
     );
-  }
-}
+  });
+
+  useEffect(() => {
+    console.log("useEffect ran - like component did mount");
+  }, []);
+  //this runs once, when the component mounts. And only runs in certain circs
+  //  you send it an array [] and is called is a dependancy array
+  // [] square brackets means it will only run once
+
+  useEffect(() => {
+    console.log("useEffect ran -  only when count changes");
+  }, [count]);
+
+  useEffect(() => {
+    console.log("useEffect ran -  only when count changes");
+  }, [otherCount]);
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Click me
+      </button>
+      <button
+        onClick={() => {
+          setOtherCount(count + 1);
+        }}
+      >
+        other Click me
+      </button>
+    </>
+  );
+};
 
 export default App;
