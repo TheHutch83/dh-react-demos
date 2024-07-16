70785bdf3749d789bc636545b70f90bb1fefba31
David Hutcheson <davidhutcheson@MacBook-Air.local>
2024-07-16 09:07:33 +0200

    basic structure - missing lifecycle, therefore is a bit hacky

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
+import Child from "./components/Child";
 
 const App = () => {
-  return <p>Hello World</p>;
+  const myRef = useRef();
+
+  const onClick = () => {
+    myRef.current.focus();
+  };
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+      }}
+    >
+      <input ref={myRef} type="text" id="demo " name="demo" />
+      <button onClick={onClick}>Set Focus</button>
+    </form>
+  );
 };
 
 export default App;
