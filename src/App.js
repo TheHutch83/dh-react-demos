23ed2ba3f1c72b27834b8c48bd8637ef113614a1
David Hutcheson <davidhutcheson@MacBook-Air.local>
2024-07-16 20:24:40 +0200

    functional simpsons API demo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const App = () => {
-  const [state, setState] = useState({ count: 0 });
+  const [simpsons, setSimpsons] = useState();
 
+  const getApiData = async () => {
+    const { data } = await axios.get(
+      `https://thesimpsonsquoteapi.glitch.me/quotes?count=50`
+    );
+    setSimpsons(data);
+
+    // setTimeout(() => {
+    //   getApiData();
+    // }, 5000);
+  };
+
+  useEffect(
+    () => {
+      getApiData();
+    },
+    [] //means return once
+  );
   return (
-    <>
-      <p>{state.count}</p>
-      <button
-        onClick={() => {
-          setState({ count: state.count + 1 });
-        }}
-      >
-        Add
-      </button>
-    </>
+    simpsons &&
+    simpsons.map((char) => {
+      return <p>{char.quote}</p>;
+    })
   );
 };
 
